2cebebd7fb8fff324fbb6820e00cc094d3513e89
민정필 <feelwjd@gmail.com>
2022-02-19 18:50:24 +0000

    change proxy

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
     devServer: {                             
       proxy: {                                // 프록시 설정
         '/api': { 
-          target: 'http://localhost:3000/api',    // 프록시 요청을 보낼 서버의 주소
+          target: 'https://52.79.212.229:3000/api',    // 프록시 요청을 보낼 서버의 주소
           changeOrigin: true, 
           pathRewrite: { 
             '^/api': ''
